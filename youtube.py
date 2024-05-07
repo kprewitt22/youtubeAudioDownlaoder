decfe4e7b57b248edaf8a639a9f14a6f72d51377
kprewitt22 <ktp132@msstate.edu>
2024-05-07 13:33:01 -0500

    Adding OAuth options, progress bars, video downloading, and enhanced menu options

--- youtube.py
+++ youtube.py
@@ -1,8 +1,12 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 import signal 
 import sys
+
 #Simple function to ignore interrupts by the user
 interrupt = False
+cacheFlag = False
+oauthFlag = False
 def sigHandler(sig, frame):
    global interrupt
    interrupt = True
@@ -38,21 +42,91 @@ def getInput():
 def downloadAudio(url):
    try:
       #Set the video object via pytube YouTube
-      video = YouTube(url)
+      video = YouTube(url,
+        on_progress_callback=on_progress,
+        use_oauth=oauthFlag,
+        allow_oauth_cache= cacheFlag
+      )
       #stream filter only the audio
       stream = video.streams.filter(only_audio=True).first()
       stream.download(filename=f"{video.title}.mp3")
       #On success print 
-      print("\nThe video is downloaded in MP3")
-   except:
-      print("\nUnable to download video please try again or a different URL")      
+      print("\nThe video is downloaded in MP3 format")
+   except Exception as error:
+      print("\nUnable to download video please try again or a different URL: ", error)
+
+def downloadVideo(url):
+   try:
+      #set the video object via Youtube from pytube
+      video = YouTube(url,
+         on_progress_callback=on_progress,
+         use_oauth=oauthFlag,
+         allow_oauth_cache= cacheFlag
+      )
+      #stream
+      stream = video.streams.filter(file_extension='mp4').first()
+      stream.download(filename=f"{video.title}.mp4")
+      print("The video was downloaded in MP4 format") 
+   except Exception as error:
+      print("\nUnable to download video please try again or a different URL: ", error)   
 signal.signal(signal.SIGINT, sigHandler)
 #Now run main script to download youtube video audio
 # Main loop
 while True:
    try:
-      url = getInput()
-      downloadAudio(url)
+      
+      print("1. Login options")
+      print("2. Download a YouTube video's audio")
+      print("3. Download a YouTube video")
+      print("4. Exit the program")
+      option = input("Please enter a menu option: ")
+      if(option == "1"):
+         while True:
+            print("1. Enable/Disable OAuth cache")
+            print("2. Enable/Disable OAuth")
+            print("3. Go back")
+            if(cacheFlag == False):
+               print("OAuth cache is currently disabled")
+            else:
+               print("OAuth cache is currently enabled")
+            if(oauthFlag == False):
+               print("OAuth is currently disabled")
+            else:
+               print("OAuth is currently enabled")
+            loginOptions = input("Please enter a menu option: ")
+            if(loginOptions == "1"):
+               if(cacheFlag == False):
+                  cacheFlag = True
+                  print("OAuth cache is now set to enabled")
+               else:
+                  cacheFlag = False
+                  print("OAuth cache is now set to disabled")
+            elif(loginOptions == "2"):
+               if(oauthFlag == False):
+                  oauthFlag = True
+                  print("OAuth is now set to enabled")
+               else:
+                  oauthFlag = False
+                  print("OAuth is now set to disabled")
+            elif(loginOptions == "3"):
+               break
+            else:
+               print("That is not a menu option please enter a valid input")
+      elif(option == "2"):
+         url = getInput()
+         downloadAudio(url)
+         print()
+      elif(option == "3"):
+         url = getInput()
+         downloadVideo(url)
+         print()
+      elif(option == "4"):
+         if keyboardInterrupt():
+            sys.exit(0)
+      else:
+         print("That is not a menu option please enter a valid input")
+         
+         
    except (KeyboardInterrupt, EOFError):
       if keyboardInterrupt():
          sys.exit(0)
